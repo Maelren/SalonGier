ecf058325fbf76a6e361f271e391fbac8df24d9d
MaciekLabedzki <lamacho123@gmail.com>
2020-05-13 19:33:17 +0200

    commit z unity

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float MouseSensitivity = 100f;
+    public Transform Player;
+    float rotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        Player.Rotate(Vector3.up * mouseX);
+
+        rotation -= mouseY;
+        rotation = Mathf.Clamp(rotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotation, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 1f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    public Transform GroundCheck;
+    public float groundDistance = .4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -1f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z) * speed * Time.deltaTime;
+
+        velocity.y += gravity * Time.deltaTime;
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        controller.Move(move);
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
