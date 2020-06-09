ba649f7c5348cd8dcea6c280805eb186e4e680e6
MaciekLabedzki <lamacho123@gmail.com>
2020-06-09 17:30:00 +0200

    Added Raycast for machines

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string SelectableTag = "selectable";
+    private Transform _selection;
+
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetChild(0).GetComponent<MeshRenderer>();
+            selectionRenderer.enabled = false;
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(SelectableTag))
+            {
+                //Debug.Log(selection.name);
+
+                var selectionRenderer = selection.GetChild(0).GetComponent<MeshRenderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.enabled = true;
+                }
+                _selection = selection;
+            }
+        }
+    }
+}
